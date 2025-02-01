0492821990148a72f2943d51dfb8b631a708a744
Danila <zanindanila@mail.ru>
2025-02-01 14:35:31 +0300

    Добавить аннотацию мапстракта

--- telemetry/collector/src/main/java/ru/practicum/mapper/ScenarioAddedEventMapper.java
+++ telemetry/collector/src/main/java/ru/practicum/mapper/ScenarioAddedEventMapper.java
@@ -1,9 +1,11 @@
 package ru.practicum.mapper;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import ru.practicum.model.hub.ScenarioAddedEvent;
 import ru.yandex.practicum.kafka.telemetry.event.ScenarioAddedEventAvro;
 
+@Mapper
 public interface ScenarioAddedEventMapper {
     ScenarioAddedEventMapper INSTANCE = Mappers.getMapper(ScenarioAddedEventMapper.class);
 
--- telemetry/collector/src/main/java/ru/practicum/mapper/ScenarioRemovedEventMapper.java
+++ telemetry/collector/src/main/java/ru/practicum/mapper/ScenarioRemovedEventMapper.java
@@ -1,9 +1,11 @@
 package ru.practicum.mapper;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import ru.practicum.model.hub.ScenarioRemovedEvent;
 import ru.yandex.practicum.kafka.telemetry.event.ScenarioRemovedEventAvro;
 
+@Mapper
 public interface ScenarioRemovedEventMapper {
     ScenarioRemovedEventMapper INSTANCE = Mappers.getMapper(ScenarioRemovedEventMapper.class);
 
